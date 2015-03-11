31c7c415b6e7488d4b3f3f524b5fee42341dff8b
Yu Hsun Lee <andylee0914.tw@gmail.com>
2015-03-11 09:42:47 +0800

    hello world

---
+++ test0311/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  test0311
+//
+//  Created by Yu Hsun Lee on 2015/3/11.
+//  Copyright (c) 2015年 Yu Hsun Lee. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main() {
+    // insert code here...
+    printf("Hello, World!\n");
+
+    return 123;
+}
