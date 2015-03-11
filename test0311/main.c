9d07b509c177cbfd8c3ccd7fc12f157847b0b9a5
Yu Hsun Lee <andylee0914.tw@gmail.com>
2015-03-11 09:54:21 +0800

    use math.h

--- test0311/main.c
+++ test0311/main.c
@@ -7,11 +7,13 @@
 //
 
 #include <stdio.h>
+#include <math.h>
 
 int main() {
     // insert code here...
     printf("Hello, World!\n");
     printf("%d\n",123);  // %d print a integer
-    printf("%f\n",230.0+456.3232+3.14159);  //%f print float num 
+    printf("%f\n",230.0+456.3232+3.14159);  //%f print float num
+    printf("%.16f\n",cos(M_PI)); //use math.h
     return 123;
 }
