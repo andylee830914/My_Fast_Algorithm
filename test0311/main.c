8bac2734cf21e55100c685af09fdb94e7401cb64
Yu Hsun Lee <andylee0914.tw@gmail.com>
2015-03-11 10:01:28 +0800

    print my name

--- test0311/main.c
+++ test0311/main.c
@@ -11,9 +11,14 @@
 
 int main() {
     // insert code here...
+    char name[50] ="Yu Hsun Lee";
+    //name be a char vector
+    //name[1], name[2],...,name[49]
     printf("Hello, World!\n");
     printf("%d\n",123);  // %d print a integer
     printf("%f\n",230.0+456.3232+3.14159);  //%f print float num
     printf("%.16f\n",cos(M_PI)); //use math.h
+    printf("%s\n",name);
+    printf("%c\n",name[10]); //print specific array
     return 123;
 }
